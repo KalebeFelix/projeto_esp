2b203addafcb7a745440b27d558017fa6218fc9e
Kalebe Felix <161886360+KalebeFelix@users.noreply.github.com>
2025-05-25 22:19:39 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,8 @@ const app = express();
 const PORT = 3000;
 
 // Substitua com suas credenciais reais do Supabase
-const SUPABASE_URL = 'https://gktxaeuuhmbqrrfhczsx.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdrdHhhZXV1aG1icXJyZmhjenN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgxMTg5ODQsImV4cCI6MjA2MzY5NDk4NH0.AYy5dKEqgGHT6c7DmMO_a2y_-RjHQRg9gKASh9v2MAA';
-
+const SUPABASE_URL = 'SUA_URL';
+const SUPABASE_KEY = 'SUA_KEY'
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Middlewares
